d089621273d9197a56ca144eb4d84bc6445d4d43
saiteja82-tech <j.saiteja7674@gmail.com>
2025-06-25 15:17:04 +0530

    HTML-BASICS

---
+++ Dom.js
@@ -0,0 +1,10 @@
+// DOM in javascript
+document.body.style.backgroundColor="white";
+Document.tittle = "DOM in javascript";
+document.getElementById("Head1").style.fontSize="50px";
+document.getElementById("Head1").style.color="Red";
+document.getElementById("para1").style.color="black";
+document.getElementById("mr").style.color="green";
+document.getElementById("mr1").style.color="orange";
+document.getElementById("mr2").style.color="black";
+document.getElementById("mr3").style.color="blue";
